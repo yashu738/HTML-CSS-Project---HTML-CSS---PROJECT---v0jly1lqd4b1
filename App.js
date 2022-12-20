8fd862eb5c7f17fe0d2b209412f9f5cd4d29c1b4
Yash Shroti <121036426+yashu738@users.noreply.github.com>
2022-12-20 20:49:21 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,15 @@
+let otp_boxes = document.querySelectorAll(".otp")
+otp_boxes[0].focus()
+otp_boxes.forEach((e, i) => {
+    e.addEventListener("keydown", (event) => {
+        if (i === otp_boxes.length - 1) {
+            return;
+        }
+        if (event.key >= 0 && event.key <= 9) {
+            setTimeout(() => {
+                otp_boxes[i + 1].focus();
+            }, 100);
 
+        }
+    });
+});
